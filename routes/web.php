cf6f32bb85f133c9136d533d55559aa41a72af23
Shah Mostafa Shawon <khanshawon1511@gmail.com>
2024-11-21 15:01:14 +0600

    1st part completed

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\BookController;
 use Illuminate\Support\Facades\Route;
-
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+Route::get("books",[BookController::class,'index'])->name('books.index');
+Route::get('books/show/{id}', [BookController::class, 'show'] );
